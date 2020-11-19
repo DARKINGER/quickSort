3312657d52188ee593d2f40dabd45da6a6ddd8bc
DARKINGER <alex10200110@gmail.com>
2020-11-19 12:23:51 -0600

    final

--- quickSort/Program.cs
+++ quickSort/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 
 namespace quickSort
 {
@@ -6,15 +7,88 @@ namespace quickSort
     {
         static void Main(string[] args)
         {
-            string arreglo = "";
-            int[] a = { 5, 3, 6, 4, 2, 9, 1, 8, 7 };
+            string arreglo = "", resultado="", resultado2 = "";
+            int[] a = { 1,2,3,4,8,6,5,4,9 }; //1, 5, 3, 6, 4, 2, 9, 1, 8, 7
+            Console.WriteLine("Arreglo precargado ");
+            for (int i = 0; i < a.Length; i++)
+            { 
+                int x = 0;
+                x = a[i];
+                Console.Write('|'+x.ToString()+'|');
+    
+            }
             QuickSort(a);
-            for (int i = 0; i < a.Length-1; i++)
+            Console.WriteLine("\n\nArreglo precargado ordenado ");
+            for (int i = 0; i < a.Length; i++)
             {
-                arreglo = arreglo + ',' + a[i];
+                arreglo = arreglo + '|' + a[i] + '|';
             }
 
             Console.WriteLine(arreglo);
+
+            //aleatorios
+
+            Console.WriteLine("\n\n\nArreglo aleatorio");
+
+            Stopwatch tim = new Stopwatch();
+            tim.Start();
+
+            Random numero = new Random();
+
+            int Tamano = numero.Next(50000, 80050);
+            int[] Arreglo = new int[Tamano];
+            Console.WriteLine("Tamaño del arreglo "+Tamano.ToString()+"\n\n");
+            for (int i = 0; i < Arreglo.Length; i++)
+            {
+                //bool bandera = false;
+                int Ente;
+                Ente = numero.Next(0, 1400);
+                for (int x = 0; x < Arreglo.Length; x++)
+                {
+                    /*
+                    if(Arreglo[x] == Ente)
+                    {
+                        bandera = false;
+                        break;
+                    }
+                    else
+                    {
+                        bandera = true;
+                    }
+                    */
+                    Arreglo[i] = Ente;
+                }
+                /*
+                if(bandera == true)
+                {
+                    Arreglo[i] = Ente;
+                }
+                else
+                {
+                    i--;
+                }
+                */
+            }
+           
+            for (int i = 0; i < Arreglo.Length; i++)
+            {
+                resultado = resultado + ',' + Arreglo[i] ;
+            }
+            Console.WriteLine(resultado);
+            QuickSort(Arreglo);
+
+            Console.WriteLine("\n\n Arreglo ya ordenado\n\n");
+
+            for (int i = 0; i < Arreglo.Length; i++)
+            {
+                resultado2 = resultado2 + ',' + Arreglo[i] ;
+            }
+            Console.WriteLine("\n"+resultado2+"\n");
+
+            Console.WriteLine("Fin del arreglo aleatorio");
+            //
+            tim.Stop();
+            Console.WriteLine($"Tiempo: {tim.Elapsed} s");
             Console.ReadKey();
 
         }
@@ -23,7 +97,7 @@ namespace quickSort
             QuickSort(a, 0, a.Length - 1);
         }
 
-        static void QuickSort(int[] a, int start, int end)
+        static void QuickSort(int[] arreglo, int start, int end)
         {
 
 
@@ -32,32 +106,35 @@ namespace quickSort
                 return;
             }
 
-            int num = a[start];
+            int num = arreglo[start];
 
             int i = start, j = end;
 
             while (i < j)
             {
-                while (i < j && a[j] > num)
+                while (i < j && arreglo[j] >= num)
                 {
                     j--;
                 }
 
-                a[i] = a[j];
+                arreglo[i] = arreglo[j];
 
-                while (i < j && a[i] < num)
+                while (i < j && arreglo[i] <= num)
                 {
                     i++;
                 }
 
-                a[j] = a[i];
+                arreglo[j] = arreglo[i];
             }
 
-            a[i] = num;
-            QuickSort(a, start, i - 1);
-            QuickSort(a, i + 1, end);
+            arreglo[i] = num;
+            QuickSort(arreglo, start, i - 1);
+            QuickSort(arreglo, i + 1, end);
 
         }
+
+
+        
     }
 }
 
